162275ed94b4dc9b068ef7ba661c50d531661662
Nicolas <nlimadasilvabrito@gmail.com>
2024-08-27 07:55:23 -0300

    feat: create addTask function

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,15 @@ let inputElement = document.querySelector("#app input") as HTMLInputElement
 let buttonElement = document.querySelector("#app button") as HTMLElement
 
 let tasks : Array<string> = []
+
+function addTask(){
+    if (inputElement.value === ""){
+        alert("Digite o nome da tarefa")
+        return false
+    } else {
+
+        let taskName : string = inputElement.value
+        tasks.push(taskName)
+        inputElement.value = ""
+    }
+}
