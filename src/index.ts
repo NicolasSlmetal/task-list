c21fa46d1d58cdfd37a8eb284c3d8928699c64cd
Nicolas <nlimadasilvabrito@gmail.com>
2024-08-27 07:53:02 -0300

    feat: add main variables to be used in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Rodando Terminal")
+let listElement = document.querySelector("#app ul") as HTMLUListElement
+let inputElement = document.querySelector("#app input") as HTMLInputElement
+let buttonElement = document.querySelector("#app button") as HTMLElement
+
+let tasks : Array<string> = []
